1f5622b248623f6d7c5add4b533e9b2ab39eb499
princessrichmond <princessdianemanalili@gmnail.com>
2024-01-22 15:19:57 -0500

    w02 task complete update

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,22 +6,21 @@
 const fullName = "Princess Richmond";
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
-// const currentYear = new Date().getFullYear() X
-const profilePicture = document.createElement("img");
-profilePicture.src = 'images/m.jpg';
+const profilePicture = "images/m.jpg";
 
 
 // /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = "images/m.jpg";
+const imageElement = document.querySelector("img");
 
 // /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong> ${fullName} </strong>`;
 yearElement.textContent = `${currentYear}`;
-// imageElement.setAttribute('src', images);
-// imageElement.setAttribute('alt', 'Profile image of $fullName');
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile Image of ${fullName}.`);
+
 
 /* Step 5 - Array */
 let food = ["Steak", "Lechon", "Tacos", "Sinigang", "Fried Chicken", "Lumpia"];
